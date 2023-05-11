715cfc3ca68b417d07df563a1d73069887d12cb2
Rodolfo ceo cortes <ceosmarck@gmail.com>
2023-05-11 12:55:29 -0600

    ok

--- block2_fbsd.py
+++ block2_fbsd.py
@@ -12,61 +12,73 @@ ip_firewall = []
 LOGFILE = "/var/log/suricata/fast.log"
 PF = "/etc/pf.conf"
 
+
 def reiniciar_pf():
     reiniciar_pf = f"service pf restart" # Reiniciamos el servicio de packet filter
     subprocess.run(reiniciar_pf, shell=True, check=True)
 
 
-def main(): 
-    file = open(LOGFILE,"r")
-    logs = file.read()
-    logs = logs.replace(" ", "")
-    logs = logs.split()
+def main():
+    while True:
+        file = open(LOGFILE,"r")
+        logs = file.read()
+        logs = logs.replace(" ", "")
+        logs = logs.split()
 
-    for i in logs:
-        if "Drop" in i:
-            direccion_ip = re.search('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', i)
-            result = direccion_ip.group(0)
-            ip_result.append(result) # Mandamos el resultado de los logs a una lista
-            #print(result)
+        for i in logs:
+            if "Drop" in i:
+                direccion_ip = re.search('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', i)
+                result = direccion_ip.group(0)
+                ip_result.append(result) # Mandamos el resultado de los logs a una lista
+                #print(result)
 
-    # Verificamos si el archivo pf.conf tiene datos existentes
+        # Verificamos si el archivo pf.conf tiene datos existentes
 
-    if os.path.getsize(PF) == 0:
-        conjunto = set(ip_result)
-        no_rep = list(conjunto)
+        if os.path.getsize(PF) == 0:
+            conjunto = set(ip_result)
+            no_rep = list(conjunto)
+            print("El archivo pf.conf está vacio. Enviando direcciones ip nuevas")
 
-        for ip_res in no_rep:
-            preparar_ip = f"block in from {ip_res}"
-            enviar_ip = f"echo {preparar_ip} >> /etc/pf.conf"
-            subprocess.run(enviar_ip, shell=True, check=True)
+            for ip_res in no_rep:
+                print(f"La dirección ip {ip_res} fue bloqueada")
+                time.sleep(1)
+                preparar_ip = f"block in from {ip_res}"
+                enviar_ip = f"echo {preparar_ip} >> /etc/pf.conf"
+                subprocess.run(enviar_ip, shell=True, check=True)
 
-        reiniciar_pf()
+            reiniciar_pf()
 
+        else:
+            with open(PF, 'r') as fw_pf: #Abrimos el archivo pf.conf de freebsd
+                contenido = fw_pf.read()
+                contenido = contenido.replace(" ", "")
+                contenido = contenido.split()
 
-    else:
-        with open(PF, 'r') as fw_pf: #Abrimos el archivo pf.conf de freebsd
-            contenido = fw_pf.read()
-            contenido = contenido.replace(" ", "")
-            contenido = contenido.split()
+                for j in contenido:
+                    block_firewall = re.search('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', j)
+                    block_firewall = block_firewall.group(0)
+                    #print(block_firewall)
+                    ip_firewall.append(block_firewall) # Mandamos los resultados del firewall a una lista                
 
-            for j in contenido:
-                block_firewall = re.search('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', j)
-                block_firewall = block_firewall.group(0)
-                #print(block_firewall)
-                ip_firewall.append(block_firewall) # Mandamos los resultados del firewall a una lista                
+            # Comparamos si hay resultados repetidos en ambas listas
+            elementos_no_repetidos = list(set(ip_result) ^ set(ip_firewall))
+            
+            if not elementos_no_repetidos:
+                print("No hay direcciones ip nuevas")
+            
+            else:
+                pass
 
-        # Comparamos si hay resultados repetidos en ambas listas
-        elementos_no_repetidos = list(set(ip_result) ^ set(ip_firewall))
-
-        for ip in elementos_no_repetidos:
-            print(f"La dirección ip {ip} fue bloqueada")
-            time.sleep(1)
-            resultado = f"block in from {ip}"
-            ip_maliciosa = f"echo {resultado} >> /etc/pf.conf"
-            subprocess.run(ip_maliciosa, shell=True, check=True)
+                for ip in elementos_no_repetidos:
+                    print(f"La dirección ip {ip} fue bloqueada")
+                    time.sleep(1)
+                    resultado = f"block in from {ip}"
+                    ip_maliciosa = f"echo {resultado} >> /etc/pf.conf"
+                    subprocess.run(ip_maliciosa, shell=True, check=True)
         
-        reiniciar_pf()
+                reiniciar_pf()
+                
+            time.sleep(10)
 
 
 if __name__ == '__main__':
